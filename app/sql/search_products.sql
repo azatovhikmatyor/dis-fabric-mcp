dce9596aa7bab05147218aa92498f415ea4c2ff8
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2026-01-07 16:35:55 +0500

    sql query update

--- app/sql/search_products.sql
+++ app/sql/search_products.sql
@@ -1,13 +1,9 @@
 SELECT
     p.product_id,
-    p.product_code,
     p.product_name,
-    p.brand,
     p.category
 FROM dim_product p
 WHERE
     LOWER(p.product_name) LIKE :query
-    OR LOWER(p.brand) LIKE :query
     OR LOWER(p.category) LIKE :query
-    OR LOWER(p.description) LIKE :query
 ORDER BY p.product_name;
